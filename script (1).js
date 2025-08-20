954c3849e44ec5c48d7424142e043f887712e0b3
AmmarKhan10 <ammarnoorudinkhan@gmail.com>
2025-08-20 21:54:28 +0500

    Delete script (1).js

--- script (1).js
+++
@@ -1,4 +0,0 @@
-const myLocation = [YOUR_LAT, YOUR_LON];
-const herLocation = [HER_LAT, HER_LON];
-
-const line = L.polyline([myLocation, herLocation], {color: 'purple', weight: 4, opacity: 0.7,'}).addTo(map);
